999d59e32eda4e078b2d0411de270aa87e2ea56b
Dominik Macher <dominikmacher@live.at>
2021-10-13 22:57:02 +0200

    working python script

--- DoorOpener/firetelegram_dooropener.py
+++ DoorOpener/firetelegram_dooropener.py
@@ -2,16 +2,17 @@ import RPi.GPIO as gpio
 import requests
 
 url = 'http://10.0.0.60/feuerwehr-karlstetten/firetelegram/?get=alarmstatus'
+ioPin = 18;
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
-gpio.setup(18, gpio.OUT)
+gpio.setup(ioPin, gpio.OUT)
 
 r = requests.get(url)
 data = r.json()
 if data["ACTIVE_ALARM"] :
   print("Active!")
-  gpio.output(18, gpio.HIGH)
+  gpio.output(ioPin, gpio.HIGH)
 else:
   print("Inactive")
-  gpio.output(18, gpio.LOW)
+  gpio.output(ioPin, gpio.LOW)
